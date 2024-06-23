8e127e0bca40f1717eeae9dc5b692662d4e2d823
Jhon Ramirez <jhonfre190@hotmail.com>
2024-06-22 23:23:47 -0500

    justify-update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import AdviceCard from "@/components/AdviceCard";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-start p-24 bg-neutral-dark-blue">
+    <main className="flex min-h-screen flex-col items-center justify-center p-24 bg-neutral-dark-blue">
       <AdviceCard/>
 
     </main>
